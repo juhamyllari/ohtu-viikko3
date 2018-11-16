9b33784a992e32180edfd64efe928bfc57ef823d
Juha Myllari <jmyllari@gmail.com>
2018-11-16 18:47:36 +0200

    complete tehtava 10

--- WebLogin/src/main/java/ohtu/Tester.java
+++ WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,33 +10,44 @@ public class Tester {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        WebElement element;
+        Random rand = new Random();
 
         driver.get("http://localhost:4567");
-        
-        sleep(2);
-        
-        WebElement element = driver.findElement(By.linkText("login"));
+        sleep(1);
+        element = driver.findElement(By.linkText("register new user"));
         element.click();
-
-        sleep(2);
-
+        sleep(1);
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("hikka" + rand.nextInt(100000));
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
-        
-        sleep(2);
-        element.submit();
+        element.sendKeys("tosisalainen0");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("tosisalainen0");
+        element = driver.findElement(By.name("signup"));
 
+        sleep(1);
+        element.submit();
+        sleep(2);
+        
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        sleep(1);
+        element.click();
         sleep(3);
         
+        element = driver.findElement(By.linkText("logout"));
+        sleep(1);
+        element.click();
+        sleep(3);
+
         driver.quit();
+
     }
-    
-    private static void sleep(int n){
-        try{
-            Thread.sleep(n*1000);
-        } catch(Exception e){}
+
+    private static void sleep(int n) {
+        try {
+            Thread.sleep(n * 1000);
+        } catch (Exception e) {
+        }
     }
 }
