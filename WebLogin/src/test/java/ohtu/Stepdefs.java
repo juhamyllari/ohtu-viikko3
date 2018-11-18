a513d9f804a21fe14bdd186d3764aef1966c3ce8
Juha Myllari <jmyllari@gmail.com>
2018-11-18 18:14:12 +0200

    complete tehtava 13

--- WebLogin/src/test/java/ohtu/Stepdefs.java
+++ WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -31,6 +31,21 @@ public class Stepdefs {
         element.click();
     }
 
+    @Given("^user with username \"([^\"]*)\" with password \"([^\"]*)\" is successfully created$")
+    public void new_account_created(String username, String password) throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        createUserWith(username, password);
+    }
+    @Given("^user with username \"([^\"]*)\" and password \"([^\"]*)\" is tried to be created$")
+    public void invalid_account_creation_attempted(String username, String password) throws Throwable {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        createUserWith(username, password);
+    }
+
     @When("^username \"([^\"]*)\" and password \"([^\"]*)\" are given$")
     public void username_and_password_are_given(String username, String password) throws Throwable {
         WebElement element = driver.findElement(By.name("username"));
@@ -55,17 +70,17 @@ public class Stepdefs {
     public void valid_username_password_matching_password_given(String username, String password) throws Throwable {
         createUserWith(username, password);
     }
-    
+
     @When("^an excessively short username \"([^\"]*)\", a valid password \"([^\"]*)\" and a matching password confirmation are entered$")
     public void short_username_valid_password_matching_password_given(String username, String password) throws Throwable {
         createUserWith(username, password);
     }
-    
+
     @When("^a valid username \"([^\"]*)\" and too short a password \"([^\"]*)\" are entered$")
     public void valid_username_short_password(String username, String password) throws Throwable {
         createUserWith(username, password);
     }
-    
+
     @When("a valid username \"([^\"]*)\", a valid password \"([^\"]*)\" and the non-matching confirmation \"([^\"]*)\" are entered$")
     public void valid_username_short_password(String username, String password, String confirmation) throws Throwable {
         createUserWith(username, password, confirmation);
@@ -91,12 +106,12 @@ public class Stepdefs {
         pageHasContent("invalid username or password");
         pageHasContent("Give your credentials to login");
     }
-    
+
     @Then("^a new user is created$")
     public void new_user_is_created() throws Throwable {
         pageHasContent("Welcome to Ohtu Application!");
     }
-    
+
     @Then("user is not created and error \"([^\"]*)\" is reported")
     public void new_user_not_created_and_error_reported(String error) throws Throwable {
         pageHasContent(error);
@@ -132,6 +147,7 @@ public class Stepdefs {
         element.sendKeys(confirmation);
         element.submit();
     }
+
     private void createUserWith(String username, String password) {
         createUserWith(username, password, password);
     }
